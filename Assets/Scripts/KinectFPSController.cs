6e97dd2f98d8d99322bf3b0fae6559d96818f649
Murad MRIMSH Khafizov <murkorp@gmail.com>
2014-09-13 20:29:43 +0400

    Calibrated and fixed controller. Also, you can play with columns!

--- Assets/Scripts/KinectFPSController.cs
+++ Assets/Scripts/KinectFPSController.cs
@@ -9,22 +9,32 @@ public class KinectFPSController : MonoBehaviour
 		public float rotSpeed = 0.25f;
 		public float deadZoneAngles = 15f;
 		public float sensorPollTime = 0.2f;
+		public Vector3 rightArmVector;
 		private Quaternion rightArmRotation;
 
+		/// <summary>
+		/// Couroutine that polls sensor for bones position every <paramref name="sensorPollTime"/> seconds. 
+		/// Also sets rotation of right arm.
+		/// </summary>
+		/// <returns>The poll couroutine.</returns>
 		IEnumerator SensorPollCouroutine ()
 		{
-				while (true) {
+				while (Application.isPlaying) {
+			
+						rightArmVector = PollSensorForVector (8, 11);
 						// Rotation from right shoulder to right hand.
-			rightArmRotation = PollSensorForLineRotation (8, 11);
+						rightArmRotation = Quaternion.FromToRotation (-Vector3.forward, rightArmVector);
 
 						yield return new WaitForSeconds (sensorPollTime);
 				}
+
+				yield return null;
 		}
 	
 		// Use this for initialization
 		void Start ()
 		{
-				StartCoroutine (SensorPollCouroutine);
+				StartCoroutine (SensorPollCouroutine ());
 		}
 	
 		// Update is called once per frame
@@ -32,37 +42,30 @@ public class KinectFPSController : MonoBehaviour
 		{
 				float rotationFactor = 0f;
 		
-				if (sw.pollSkeleton ()) {
-						Vector3 direction = GetArmDirection ();
-						Quaternion armRotation = Quaternion.FromToRotation (-Vector3.forward, direction);
-
-						if (Mathf.Abs (armRotation.eulerAngles.y - 180f) > deadZoneAngles) {
-								rotationFactor = (armRotation.eulerAngles.y - 180f);
-						}
-
-						Debug.Log (armRotation.eulerAngles + "; " + rotationFactor);
-			
-						rigidbody.velocity = transform.forward * speed;
+				if (Mathf.Abs (rightArmRotation.eulerAngles.y - 180f) > deadZoneAngles) {
+						rotationFactor = (rightArmRotation.eulerAngles.y - 180f);
 				}
+		
+				Debug.Log (rightArmRotation.eulerAngles + "; " + rotationFactor);
+		
+				rigidbody.velocity = transform.forward * speed;
 
 				rigidbody.angularVelocity = new Vector3 (0f, rotationFactor * rotSpeed, 0f);
 		}
 
 		/// <summary>
-		/// Polls the sensor and gets rotation of line between two bones.
+		/// Polls the sensor and gets vector of line between two bones.
 		/// </summary>
-		/// <returns>Line rotation.</returns>
-		private Quaternion PollSensorForLineRotation (int firstBone, int secondBone)
+		/// <returns>Line between two bones.</returns>
+		private Vector3 PollSensorForVector (int firstBone, int secondBone)
 		{
 				if (sw.pollSkeleton ()) {
 						// Get the arm direction. It's a normalized vector from first bone to second.
-						Vector3 armDirection = (sw.bonePos [player, secondBone] - sw.bonePos [player, firstBone]).normalized;
-						// Get rotation of the line.
-						Quaternion armRotation = Quaternion.FromToRotation (-Vector3.forward, armDirection);
+						Vector3 lineDirection = sw.bonePos [player, secondBone] - sw.bonePos [player, firstBone];
 
-						return armRotation;
+						return lineDirection;
 				}
 
-				return null;
+				return Vector3.zero;
 		}
 }
