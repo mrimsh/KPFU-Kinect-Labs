c3dea5457b93dff8e9cd76d1457a62bd81e3796d
Murad "MRIMSH" Khafizov <murkorp@gmail.com>
2014-09-27 16:22:59 +0400

    All levels done.

---
+++ Assets/Scripts/L5.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class L5 : MonoBehaviour
+{
+		public Transform point1;
+		public Transform point2;
+		public Transform player;
+		public Vector3 offset;
+		private bool isPoint1 = true;
+
+		// Use this for initialization
+		void Start ()
+		{
+		
+				player.position = point1.position + offset;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						if (isPoint1) {
+								player.position = point2.position + offset;
+						} else {
+								player.position = point1.position + offset;
+						}
+						isPoint1 = !isPoint1;
+				}
+		}
+}
